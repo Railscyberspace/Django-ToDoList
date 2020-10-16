acbd92a075b00468793fc514012a423221b1534d
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2020-10-16 08:51:35 +0100

    commit

--- main/views.py
+++ main/views.py
@@ -7,6 +7,6 @@ from .models import ToDoList, Item
 def index(response, name):
     ls = ToDoList.objects.get(name=name)
     Item = Is.item_set.get('id=1')
-    return HttpResponse('<h1>%s</h1><br></br><p>%</p>'% str(ls.name,item.text)
+    return HttpResponse('<h1>%s</h1><br></br><p>%</p>' %(ls.name,str(item.text)))
 
 
