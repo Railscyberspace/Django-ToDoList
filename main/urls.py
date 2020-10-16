db8d47080bfe64e9ae176bee299d2f63094c905a
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2020-10-16 08:34:38 +0100

    first change

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .import views
-
+from . import views
 
 urlpatterns = [
     path('<int:id>', views.index, name='index'),
--- main/views.py
+++ main/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ToDoList, Item
 # Create your views here.
 
 
-def index(response, id):
-    ls = ToDoList.objects.get(id=id)
-    return HttpResponse('<h1>%s</h1>' % ls.name)
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    Item = Is.item_set.get('id=1')
+    return HttpResponse('<h1>%s</h1><br></br><p>%</p>'% str(ls.name,item.text)
 
 
